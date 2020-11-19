f57b1bae3f78795d51a283499c57b4fe6f54e8d8
Navpreetkaur-git <navbana08@gmail.com>
2020-11-19 20:23:50 +0100

    First commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  
+  selector: 'app-root',
+  template: `
+
+   <h1> {{7+6}}</h1>
+
+  `,
+  styleUrls:   ['./app.component.css']
+  
+})
+export class AppComponent {
+public name='navpreet';
+}
